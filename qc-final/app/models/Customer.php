f1a1c8168edd6e2a731578168c7751e2c8a201b6
Ammar Tubagus <cantikakusuma370@gmail.com>
2025-11-04 09:18:50 +0700

    Update Customer.php

--- qc-final/app/models/Customer.php
+++ qc-final/app/models/Customer.php
@@ -1,11 +1,19 @@
 <?php
-class Customer{
-    private $conn; private $table='customer';
-    public $customer_id; public $costume_id; public $customer_name; public $gender; public $email; public $phone_number; public $address;
-    public function __construct($db){ $this->conn=$db; }
-    public function all(){ $stmt = $this->conn->query("SELECT * FROM {$this->table}"); return $stmt->fetchAll(PDO::FETCH_ASSOC); }
-    public function find($id){ $stmt = $this->conn->prepare("SELECT * FROM {$this->table} WHERE customer_id=:id"); $stmt->execute(array(':id'=>$id)); return $stmt->fetch(PDO::FETCH_ASSOC); }
-    public function create(){ $sql="INSERT INTO {$this->table} (costume_id, customer_name, gender, email, phone_number, address) VALUES (:costume_id,:name,:gender,:email,:phone,:address)"; $stmt=$this->conn->prepare($sql); return $stmt->execute(array(':costume_id'=>$this->costume_id,':name'=>$this->customer_name,':gender'=>$this->gender,':email'=>$this->email,':phone'=>$this->phone_number,':address'=>$this->address)); }
-    public function update($id){ $sql="UPDATE {$this->table} SET costume_id=:costume_id, customer_name=:name, gender=:gender, email=:email, phone_number=:phone, address=:address WHERE customer_id=:id"; $stmt=$this->conn->prepare($sql); return $stmt->execute(array(':costume_id'=>$this->costume_id,':name'=>$this->customer_name,':gender'=>$this->gender,':email'=>$this->email,':phone'=>$this->phone_number,':address'=>$this->address,':id'=>$id)); }
-    public function delete($id){ $stmt=$this->conn->prepare("DELETE FROM {$this->table} WHERE customer_id=:id"); return $stmt->execute(array(':id'=>$id)); }
+require_once _DIR_ . '/../config/Database.php';
+require_once _DIR_ . '/../models/costume.php';
+
+class CostumeController {
+    private $model;
+
+    public function __construct() {
+        $database = new Database();
+        $db = $database->getConnection();
+        $this->model = new Costume($db);
+    }
+
+    public function index() {
+        $data = $this->model->getAll();
+        include _DIR_ . '/../views/costume_views.php';
+    }
 }
+?>
